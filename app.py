3e9e1d04ffab5fe30b0438eb00fcb61b031e9050
anchit <anchitmandal@gmail.com>
2025-03-18 21:32:32 +0530

    modified:   .gitignore 	modified:   app.py 	new file:   models.py 	new file:   requirements.txt 	new file:   templates/page1.html

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from models import db
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.sqlite3'
+
+db.init_app(app)
 
 @app.route('/')
 def hello_world():
@@ -11,6 +15,15 @@ def hello_world():
 @app.route('/html')
 def hello_html():
     return render_template('index.html')
+
+@app.route('/page1', methods=['GET', 'POST'])
+def page01():
+    if request.method == 'GET':
+        return render_template('page1.html')
+    if request.method == 'POST':
+        print(request.form) 
+        return redirect(url_for('page01'))
+
 
 if __name__ == '__main__':
     app.run(port=1508)
