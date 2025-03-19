0419dd29b1231bd4d2de6ede5de4414958b8e648
anchit <anchitmandal@gmail.com>
2025-03-19 20:42:47 +0530

    modified:   app.py 	modified:   models.py

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template, request, redirect, url_for
-from models import db
+from models import db, user
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.sqlite3'
 
 db.init_app(app)
+
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def hello_world():
@@ -21,7 +24,10 @@ def page01():
     if request.method == 'GET':
         return render_template('page1.html')
     if request.method == 'POST':
-        print(request.form) 
+        name_var = request.form['username']
+        new_user = user(name=name_var)
+        db.session.add(new_user)
+        db.session.commit()
         return redirect(url_for('page01'))
 
 
--- models.py
+++ models.py
@@ -2,3 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class user(db.Model):
+    __tablename__ = 'User'
+    user_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
