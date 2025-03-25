94208c8cbf12d8a1f2bff91a919d70b92425b5fc
anchit <anchitmandal@gmail.com>
2025-03-25 22:22:47 +0530

    modified:   app.py 	modified:   models.py 	modified:   requirements.txt 	new file:   templates/login.html 	modified:   templates/page3.html 	new file:   templates/register.html

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
 from flask import Flask, render_template, request, redirect, url_for
-from models import db, user
+from flask_security import Security, login_user, logout_user, current_user, auth_required, roles_accepted
+
+from models import db, user, user_datastore
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.sqlite3'
+app.config['SECRET_KEY'] = 'super-secret'
+app.config['SECURITY_LOGIN_URL'] = '/ijghjsvef67832r'
+app.config['SECURITY_UNAUTHORIZED_VIEW'] = '/login'
 
 db.init_app(app)
+Security(app, user_datastore)
 
 with app.app_context():
     db.create_all()
+    user_datastore.find_or_create_role(name='admin')
+    user_datastore.find_or_create_role(name='user')
+    if not user_datastore.find_user(email="admin@a.com"):
+        user_datastore.create_user(email="admin@a.com", password="admin", name="admin", roles=['admin'])
+    db.session.commit()
 
 @app.route('/')
 def hello_world():
@@ -20,6 +31,8 @@ def hello_html():
     return render_template('index.html')
 
 @app.route('/page1', methods=['GET', 'POST'])
+@auth_required('session')
+@roles_accepted('admin')
 def page01():
     if request.method == 'GET':
         return render_template('page1.html')
@@ -47,6 +60,43 @@ def page03():
     all_data = user.query.all()
     print(all_data)
     return render_template('page3.html', data=all_data)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user = user_datastore.find_user(email=email)
+        if user and user.password == password:
+            login_user(user)
+            return redirect(url_for('page01'))
+        else:
+            return 'Invalid credentials'
+        
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+    
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        name = request.form['name']
+        # print(email, password, name)
+        # new_user = user(email=email, password=password, name=name)
+        # db.session.add(new_user)
+        if not user_datastore.find_user(email=email):
+            user_datastore.create_user(email=email, password=password, name=name, roles=['user'])
+            db.session.commit()
+            return redirect(url_for('login'))
+        else:
+            return 'User already exists, go back and try again'
 
 if __name__ == '__main__':
     app.run(port=1508)
--- models.py
+++ models.py
@@ -1,8 +1,28 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_security import UserMixin, RoleMixin, SQLAlchemyUserDatastore
 
 db = SQLAlchemy()
 
-class user(db.Model):
+class user(db.Model, UserMixin):
     __tablename__ = 'User'
-    user_id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
+    id = db.Column(db.Integer, primary_key=True) #
+    email = db.Column(db.String(120), unique=True, nullable=False) #
+    password = db.Column(db.String(120), nullable=False) #
+    name = db.Column(db.String(120), nullable=False)
+    active = db.Column(db.Boolean, default=True) #
+    fs_uniquifier = db.Column(db.String(120), nullable=False) #
+    roles = db.relationship('role', secondary='RolesUser', backref=db.backref('users', lazy='dynamic')) #
+
+class role(db.Model, RoleMixin):
+    __tablename__ = 'Role'
+    id = db.Column(db.Integer, primary_key=True) #
+    name = db.Column(db.String(120), nullable=False) #
+
+class RolesUser(db.Model):
+    __tablename__ = 'RolesUser'
+    id = db.Column(db.Integer, primary_key=True) #
+    user_id = db.Column(db.Integer, db.ForeignKey('User.id')) #
+    role_id = db.Column(db.Integer, db.ForeignKey('Role.id')) #
+
+user_datastore = SQLAlchemyUserDatastore(db, user, role)
+
